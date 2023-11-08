64f4848617812998b7ff77718076b7f7694c532e
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-08 22:50:13 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,54 @@
-import "./App.css";
+import { useState } from "react";
 import { useGetPokemonByNameQuery } from "./services/pokemon";
 
 function App() {
-  const { data, error, isLoading } = useGetPokemonByNameQuery('ivysaur');
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const { data, error, isLoading } = useGetPokemonByNameQuery(
+    "pokemon/?offset=20&limit=20"
+  );
+
+  const handlePokemonClick = async () => {
+    try {
+     
+      setSelectedPokemon(data);
+    } catch (error) {
+      // Handle any errors here
+      console.error("Error fetching Pokemon details:", error);
+    }
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
-    <div className="App">
-      {error ? (
-        <>Oh no, there was an error</>
-      ) : isLoading ? (
-        <>Loading...</>
-      ) : data ? (
-        <>
-          <h3>{data.species.name}</h3>
-          <img src={data.sprites.back_default} alt={data.species.name} />
-        </>
-      ) : null}
+    <div>
+      <h1>Pokemon List</h1>
+      <ul>
+        {data.results.map((pokemon) => (
+          <>
+            <li key={pokemon.name}>
+              <button
+               
+                onClick={() => handlePokemonClick(pokemon.name)}
+              >
+                {pokemon.name}
+              </button>
+            </li>
+          </>
+        ))}
+      </ul>
+
+      {selectedPokemon && (
+        <div>
+          <h2>Selected Pokemon: {selectedPokemon.name}</h2>
+          {/* Display other details of the selected Pokemon here */}
+        </div>
+      )}
     </div>
   );
 }
--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -5,7 +5,7 @@ export const pokemonApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
   endpoints: (builder) => ({
     getPokemonByName: builder.query({
-      query: (name) => `pokemon/${name}`,
+      query: (url) => url,
     }),
   }),
 });
