5077656fcfa5641f427f0b1dc253730d4f1f8e27
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-08 22:32:01 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-
-import './App.css'
+import "./App.css";
+import { useGetPokemonByNameQuery } from "./services/pokemon";
 
 function App() {
- 
+  const { data, error, isLoading } = useGetPokemonByNameQuery('ivysaur');
 
   return (
-    <>
-      <h1>Hi!</h1>
-    </>
-  )
+    <div className="App">
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species.name}</h3>
+          <img src={data.sprites.back_default} alt={data.species.name} />
+        </>
+      ) : null}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -2,15 +2,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const pokemonApi = createApi({
   reducerPath: "pokemonApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://pokeapi.co/api/v2/",
-  }),
-  endpoints: (build) => ({
-    qetPokemonByName: build.query({
-      query: (name) => `pokemon${name}`,
+  baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+  endpoints: (builder) => ({
+    getPokemonByName: builder.query({
+      query: (name) => `pokemon/${name}`,
     }),
   }),
 });
 
-
-export const {useLazyQetPokemonByNameQuery} = pokemonApi
+export const { useGetPokemonByNameQuery } = pokemonApi;
